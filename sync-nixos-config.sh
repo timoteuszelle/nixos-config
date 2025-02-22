67b002d55050f425abab9be5f8b9fed85c897c1a
Tim Oudesluijs-Zelle <tim@oudesluijszelle.nl>
2025-02-22 23:30:01 +0100

    chore: remove unused example and sync files

--- sync-nixos-config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Sync individual configuration files
-cp /etc/nixos/{configuration.nix,hardware-configuration.nix,homeassistant.nix,ollama.nix,pihole.nix,plex.nix,prometheus.nix,qbittorrent.nix,portainer.nix,hokkaido.nix,netboot.nix} ~/nixos-config/
-
-# Sync modules directory
-mkdir -p /etc/nixos/modules
-cp -r /etc/nixos/modules/* ~/nixos-config/modules/
-
-echo "NixOS configuration files synced successfully!"
